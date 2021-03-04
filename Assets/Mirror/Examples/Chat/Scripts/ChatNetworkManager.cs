4c3913a6f169cae8fb2b8ad4f3fe198b61ab67ba
M. S. Farzan <doryoku@gmail.com>
2021-03-03 16:19:15 -0800

    Initial commit.

---
+++ Assets/Mirror/Examples/Chat/Scripts/ChatNetworkManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace Mirror.Examples.Chat
+{
+    [AddComponentMenu("")]
+    public class ChatNetworkManager : NetworkManager
+    {
+        [Header("Chat GUI")]
+        public ChatWindow chatWindow;
+
+        public string PlayerName { get; set; }
+
+        public void SetHostname(string hostname)
+        {
+            networkAddress = hostname;
+        }
+
+        public struct CreatePlayerMessage : NetworkMessage
+        {
+            public string name;
+        }
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+            NetworkServer.RegisterHandler<CreatePlayerMessage>(OnCreatePlayer);
+        }
+
+        public override void OnClientConnect(NetworkConnection conn)
+        {
+            base.OnClientConnect(conn);
+
+            // tell the server to create a player with this name
+            conn.Send(new CreatePlayerMessage { name = PlayerName });
+        }
+
+        void OnCreatePlayer(NetworkConnection connection, CreatePlayerMessage createPlayerMessage)
+        {
+            // create a gameobject using the name supplied by client
+            GameObject playergo = Instantiate(playerPrefab);
+            playergo.GetComponent<Player>().playerName = createPlayerMessage.name;
+
+            // set it as the player
+            NetworkServer.AddPlayerForConnection(connection, playergo);
+
+            chatWindow.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class DrawCards : NetworkBehaviour
+{
+    public PlayerManager PlayerManager;
+
+    public void OnClick()
+    {
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        PlayerManager = networkIdentity.GetComponent<PlayerManager>();
+        PlayerManager.CmdDealCards();
+    }
+
+}
